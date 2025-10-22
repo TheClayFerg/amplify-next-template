498b5dfcf84c2676e2c78073674c5bbb31ff431e
TheClayFerg <claytonferguson55@yahoo.com>
2025-10-22 17:18:54 -0500

    Added about us page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+﻿"use client";
+import Image from "next/image";
+
+export default function About() {
+    return (
+        <div>
+            <main className="p-10 text-center flex flex-col items-center">
+                <h1 className="text-4xl font-bold mb-4 text-green-600">About Us</h1>
+                <p className="text-lg text-gray-700 mb-6">
+                    My name is Clay and I like to play racquet sports and code.
+                </p>
+            </main>
+        </div>
+    );
+}
